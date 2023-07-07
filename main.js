d7df23ee7174c9f473cd7e4d2cd6fcc595bcd4fb
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-07-07 12:56:31 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -3,50 +3,62 @@
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
-      return userInput;
+        return userInput;
     }else {
-      console.log('What game are you playing?')
+        console.log('What game are you playing?')
     }
-  };
+};
   
-  // computer choice
+// computer choice
   
-  const getComputerChoice = () => {
+const getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-      return 'rock';
+        return 'rock';
     }else if (randomNumber === 1) {
-      return 'paper';
+        return 'paper';
     }else if (randomNumber === 2) {
-      return 'scissors';
+        return 'scissors';
     }
-  }
+}
 
-  // determine the winner
+// determine the winner
   
-  const determineWinner = (userChoice, computerChoice) => {
+const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === 'bomb') {
-      return 'Winner winner chicken dinner!';
+        return 'Winner winner chicken dinner!';
     }
     if (userChoice === computerChoice) {
-      return 'We have a tie! Try again.';
-    } else if (userChoice === 'rock') {
-        if (computerChoice === 'paper') {
-          return 'LOOOOSSEEEERRR!';
-        } else {
-          return 'Winner winner chicken dinner!';
-        }
-    }else if (userChoice === 'paper') {
-        if (computerChoice === 'scissors') {
-          return 'LOOOOSSEEEERRR!';
-        } else {
-          return 'Winner winner chicken dinner!';
-        }
-    } else if (userChoice === 'scissors') {
-        if (computerChoice === 'rock') {
-          return 'LOOOOSSEEEERRR!';
-        } else {
-          return 'Winner winner chicken dinner!';
-        }
+        return 'We have a tie! Try again.';
+        } else if (userChoice === 'rock') {
+            if (computerChoice === 'paper') {
+                return 'LOOOOSSEEEERRR!';
+            } else {
+                return 'Winner winner chicken dinner!';
+            }
+        }else if (userChoice === 'paper') {
+            if (computerChoice === 'scissors') {
+                return 'LOOOOSSEEEERRR!';
+            } else {
+                return 'Winner winner chicken dinner!';
+            }
+        } else if (userChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                return 'LOOOOSSEEEERRR!';
+            } else {
+                return 'Winner winner chicken dinner!';
+            }
     }
-  }
+}
+
+// play the game
+  
+const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw:' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+};
+  
+playGame();
